9431651d469553b99a528fab645efad665565280
K A B I L <100506503+Kabilduke@users.noreply.github.com>
2023-12-12 20:20:33 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,9 @@
+def welcome_to_organization():
+    # Get your friend's name
+    friend_name = input("Enter your friend's name: ")
+
+    # Print a personalized welcome message
+    print(f"Welcome to the organization, {friend_name}! We're thrilled to have you on board.")
+
+# Call the function to execute the welcome process
+welcome_to_organization()
